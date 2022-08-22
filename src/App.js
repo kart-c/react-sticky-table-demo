9b3616ea743f918b1defb12b51363a0a7ce85265
Kartik Choudhary <kartikeya@thegeeksquad.tech>
2022-08-22 19:25:55 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { StickyTable, Row, Cell } from "react-sticky-table";
+import { useEffect, useRef } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const ref = useRef();
+
+	useEffect(() => {
+		console.dir(ref);
+	}, []);
+
+	return (
+		<div className="App" style={{ width: 500 }}>
+			<StickyTable leftStickyColumnCount={2}>
+				<Row>
+					<Cell>Header 1</Cell>
+					<Cell ref={ref}>Header 2</Cell>
+					<Cell>Header 3</Cell>
+					<Cell>Header 4</Cell>
+					<Cell>Header 5</Cell>
+					<Cell>Header 1</Cell>
+					<Cell>Header 2</Cell>
+					<Cell>Header 3</Cell>
+					<Cell>Header 4</Cell>
+					<Cell>Header 5</Cell>
+				</Row>
+				<Row>
+					<Cell>Cell 1</Cell>
+					<Cell>Cell 2</Cell>
+					<Cell>Cell 3</Cell>
+					<Cell>Cell 4</Cell>
+					<Cell>Cell 5</Cell>
+					<Cell>Cell 1</Cell>
+					<Cell>Cell 2</Cell>
+					<Cell>Cell 3</Cell>
+					<Cell>Cell 4</Cell>
+					<Cell>Cell 5</Cell>
+				</Row>
+				<Row>
+					<Cell>Cell 1</Cell>
+					<Cell>Cell 2</Cell>
+					<Cell>Cell 3</Cell>
+					<Cell>Cell 4</Cell>
+					<Cell>Cell 5</Cell>
+					<Cell>Cell 1</Cell>
+					<Cell>Cell 2</Cell>
+					<Cell>Cell 3</Cell>
+					<Cell>Cell 4</Cell>
+					<Cell>Cell 5</Cell>
+				</Row>
+				<Row>
+					<Cell>Cell 1Cell 1Cell 1Cell 1Cell 1</Cell>
+					<Cell>Cell 2</Cell>
+					<Cell>Cell 3</Cell>
+					<Cell>Cell 4</Cell>
+					<Cell>Cell 5</Cell>
+					<Cell>Cell 1</Cell>
+					<Cell>Cell 2</Cell>
+					<Cell>Cell 3</Cell>
+					<Cell>Cell 4</Cell>
+					<Cell>Cell 5</Cell>
+				</Row>
+			</StickyTable>
+		</div>
+	);
 }
 
 export default App;
